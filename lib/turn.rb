09c8a17632fa080046b35c3ecb92e7e2599ce581
Sebastian Waetzold <swaetzold@gmx.de>
2019-08-08 14:03:16 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,20 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  if index <= 8 && index >= 0
+    true
+  else
+    if board[index] == "X" || board[index] == "O"
+      false
+    else
+      true
+    end
+    false
+  end
+end
